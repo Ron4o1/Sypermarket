7d1e394d8e7040e58f1a7727078a465ef2cff3cc
RaynaHristovaa <rayna_hristova82@abv.bg>
2026-07-06 18:10:23 +0300

    Add properties and constructors to Product class
    Added properties for ProductID, Name, Category, Price, and Quantity. Included constructors for default and parameterized initialization.

--- Sypermarket/Product.cs
+++ Sypermarket/Product.cs
@@ -8,5 +8,37 @@ namespace Sypermarket
 {
     internal class Product
     {
+
+        public string ProductID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public double Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public Product()
+        {
+
+        }
+
+        public Product(string id, string name, string category, double price, int quantity)
+        {
+            ProductID = id;
+            Name = name;
+            Category = category;
+            Price = price;
+            Quantity = quantity;
+        }
+
+
+
+
+
+
+
+        
     }
 }
