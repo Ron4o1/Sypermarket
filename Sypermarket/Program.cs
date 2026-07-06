f62effec8b139752350f8ec454c97fca0269fc4e
Ron4o1 <veronika2009roni@gmail.com>
2026-07-06 14:27:25 +0300

    Added a reading menu

--- Sypermarket/Program.cs
+++ Sypermarket/Program.cs
@@ -10,7 +10,20 @@ namespace Sypermarket
         static string fileName = "products.txt";
         static void Main(string[] args)
         {
-           
+           List<Product> products = LoadProducts(fileName);
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("--- ХРАНИТЕЛЕН МАГАЗИН ---");
+                Console.WriteLine("1. Добавяне на продукт");
+                Console.WriteLine("2. Продажба на продукт");
+                Console.WriteLine("3. Проверка на продукт");
+                Console.WriteLine("4. Справка на наличност");
+                Console.WriteLine("5. Изход");
+                Console.WriteLine("Избор:");
+            }
+            
         }
     }
 }
