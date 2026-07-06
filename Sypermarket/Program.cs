551899a0650591688f2f7d5bb2db2ac7a46a13f3
Ron4o1 <veronika2009roni@gmail.com>
2026-07-06 14:19:37 +0300

    Added a static string products.tx

--- Sypermarket/Program.cs
+++ Sypermarket/Program.cs
@@ -1,7 +1,13 @@
-﻿namespace Sypermarket
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Sypermarket
 {
     internal class Program
     {
+        static string fileName = "products.txt";
         static void Main(string[] args)
         {
            
